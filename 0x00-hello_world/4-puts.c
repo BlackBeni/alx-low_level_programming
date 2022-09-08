c2f8d3cea2e0cd4300ac4a471d2a079e51538cb0
BlackBeni <pierrebenitomarie@gmail.com>
2022-09-08 15:33:27 -0700

    Task4 hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-/* print
- * main 
- * ret */
+#include<stdio.h>
+/*
+ * print
+ * main - boff²
+ * ret
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
